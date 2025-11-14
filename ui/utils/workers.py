3a55523e5d8064fc85e808b779e5f35e518d21c2
Sanketh <sankethsequeira@gmail.com>
2025-11-14 09:51:45 -0600

    fixed errors

--- ui/utils/workers.py
+++ ui/utils/workers.py
@@ -1,4 +1,5 @@
 from PyQt6.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot
+from PyQt6.QtWidgets import QApplication
 
 
 class WorkerSignals(QObject):
@@ -13,15 +14,35 @@ class FunctionWorker(QRunnable):
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
-        self.signals = WorkerSignals()
+        # Parent signals to the QApplication instance to keep the underlying
+        # C++ QObject alive for the duration of the application. This prevents
+        # 'wrapped C/C++ object ... has been deleted' when emitting from threads.
+        app = QApplication.instance()
+        if app is not None:
+            self.signals = WorkerSignals(parent=app)
+        else:
+            # Fallback: if QApplication isn't created yet, keep an unparented
+            # WorkerSignals. In practice the UI creates the QApplication before
+            # workers are used.
+            self.signals = WorkerSignals()
 
     @pyqtSlot()
     def run(self):
         try:
             res = self.fn(*self.args, **self.kwargs)
         except Exception as e:
-            self.signals.error.emit(e)
+            try:
+                self.signals.error.emit(e)
+            except RuntimeError:
+                # signals QObject was deleted during shutdown; ignore
+                pass
         else:
-            self.signals.result.emit(res)
+            try:
+                self.signals.result.emit(res)
+            except RuntimeError:
+                pass
         finally:
-            self.signals.finished.emit()
+            try:
+                self.signals.finished.emit()
+            except RuntimeError:
+                pass
